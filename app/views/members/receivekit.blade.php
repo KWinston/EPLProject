d916f0fdb25e3ec4036f69d057b70babca94fbad
Winston Kouch <KWinston05@gmail.com>
2015-03-01 17:08:47 -0700

    Revision to power points and template made for receive kit

--- app/views/members/receivekit.blade.php
+++ app/views/members/receivekit.blade.php
@@ -1,8 +1,124 @@
 @extends('layouts.master')
+
 @section('head')
 
 @stop
 
 @section('content')
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg .tg-c7d0{background-color:#bbdaff}
+</style>
+<br>
+<p> Dell Laptop Kit #1 </p>
+<table class="tg">
+  <tr>
+    <th class="tg-c7d0">Item</th>
+    <th class="tg-c7d0">Serial Number</th>
+    <th class="tg-c7d0">Comments</th>
+    <th class="tg-c7d0">Damaged</th>
+    <th class="tg-c7d0">Missing</th>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #1</td>
+    <td class="tg-031e">1234ABCD</td>
+    <td class="tg-031e">The spacebar is loose on one of the laptops</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e">One mouse is missing</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #2</td>
+    <td class="tg-031e">2134DDEB</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #3</td>
+    <td class="tg-031e">4312ABDE</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+</table>
 
+       </td>
+    </tr>
+
+</tr>
+</table>
+<br>
+<INPUT TYPE = "Submit" Name = "SubmitBtn" Value = "Confirm Kit is Received">
+
+<br>
+<br>
+<p> Dell Laptop Kit #2 </p>
+<table class="tg">
+  <tr>
+    <th class="tg-c7d0">Item</th>
+    <th class="tg-c7d0">Serial Number</th>
+    <th class="tg-c7d0">Comments</th>
+    <th class="tg-c7d0">Damaged</th>
+    <th class="tg-c7d0">Missing</th>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #1</td>
+    <td class="tg-031e">1234OTCD</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #2</td>
+    <td class="tg-031e">2134DEEJ</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Dell Laptop #3</td>
+    <td class="tg-031e">4312AFDE</td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+  </tr>
+</table>
+
+       </td>
+    </tr>
+
+</tr>
+</table>
+<br>
+<INPUT TYPE = "Submit" Name = "SubmitBtn" Value = "Confirm Kit is Received">
 @stop
