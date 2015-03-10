8ad67b334a67cd1261eab60112077d44a4fa1f33
robert Babiak <babiakr3@mymail.macewan.ca>
2015-03-09 18:16:30 -0600

    Added the kit overview status page. This will list all kits and there current status as a column break by kit type.

--- app/controllers/OverviewKitController.php
+++ app/controllers/OverviewKitController.php
@@ -1,10 +1,24 @@
 <?php
 include('Globals/GlobalFunctions.php');
 
-class OverviewKitController extends BaseController {
+clASs OverviewKitCONtroller extends BaseController {
 
-	function index()
+	functiON index()
 	{
-		return CheckIfAuthenticated('members.overviewkit',[ 'selected_menu' => 'main-menu-overview'], 'home.index', [], false);
+        $data = DB::select( 'SELECT K.ID AS KitID, KT.Name AS KitType, K.Name AS KitName, B.BranchID AS BranchName, KS.StateName AS KitState, BK.StartDate, B2.BranchID AS ForBranchName, count(BK.ID) AS BookingCount
+                              FROM kits AS K
+                                INNER JOIN KitTypes AS KT ON (KT.ID = K.KitType)
+                                INNER JOIN Branches AS B ON (B.ID = K.AtBranch)
+                                INNER JOIN KitState AS KS ON (KS.ID = K.KitState)
+                                LEFT JOIN  Booking AS BK ON (BK.KitID = K.ID AND BK.StartDate > now())
+                                    LEFT JOIN Branches AS B2 ON (B2.ID = BK.ForBranch)
+                              WHERE K.Available = true
+                              GROUP BY K.ID, KT.Name, K.Name, B.Name, B.BranchID
+                              ORDER BY KT.Name, K.Name, BK.StartDate DESC
+                            ;');
+		return CheckIfAuthenticated('members.overviewkit',
+            [  'selected_menu' => 'main-menu-overview',
+               'data' => $data
+            ], 'home.index', [], false);
 	}
 }
--- app/views/members/overviewkit.blade.php
+++ app/views/members/overviewkit.blade.php
@@ -4,5 +4,43 @@
 @stop
 
 @section('content')
+<table class="kit-status">
+    <tr class="kit-status">
+        <th class="kit-status kit-type">Kit Type</th>
+        <th class="kit-status kit-name">Kit Name</th>
+        <th class="kit-status branch-id">At Branch</th>
+        <th class="kit-status kit-state">Kit State</th>
+        <th class="kit-status start-date">Next Booking Starts</th>
+        <th class="kit-status branch-name">Booked For </th>
+        <th class="kit-status booking-count">Total Future Bookings</th>
+    </tr>
+    {{$breakKitType = null }}
+
+    @foreach($data as $row)
+    <tr class="kit-status">
+        @if($row->KitType != $breakKitType)
+            <td class="kit-status kit-type">{{$row->KitType}}</td>
+        @else
+            <td class="kit-status kit-type"></td>
+        @endif
+        <td class="kit-status kit-name">{{$row->KitName}}</td>
+        <td class="kit-status branch-id">{{$row->BranchName}}</td>
+        <td class="kit-status kit-state">{{$row->KitState}}</td>
+        @if(isset($row->StartDate))
+            <td class="kit-status start-date">{{ date("D d-F-Y",strtotime($row->StartDate)) }}</td>
+        @else
+            <td class="kit-status start-date"></td>
+        @endif
+        <td class="kit-status branch-name">{{$row->ForBranchName}}</td>
+        @if($row->BookingCount > 0)
+            <td class="kit-status booking-count">{{$row->BookingCount}}</td>
+        @else
+            <td class="kit-status booking-count"></td>
+        @endif
+
+        <!-- {{$breakKitType = $row->KitType;}} -->
+    </tr>
+    @endforeach
+</table>
 
 @stop
