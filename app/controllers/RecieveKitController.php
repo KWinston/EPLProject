4b13869d0150b6a323380fb5c8be94c9158c057b
Winston Kouch <KWinston05@gmail.com>
2015-03-10 14:54:26 -0600

    Fix SQL Entries and grab booking information

--- app/controllers/RecieveKitController.php
+++ app/controllers/RecieveKitController.php
@@ -6,18 +6,45 @@ class RecieveKitController extends BaseController {
 	public function index()
 	{
         $index = Session::All();
+        $post = Input::except('ID');
+        $branch = Branches::find(Session::get('branch'));
 
         $receiveKits = DB::table('Booking')
             ->join('BookingDetails',
                 'Booking.ID', '=', 'BookingDetails.BookingID')
+            ->join('Kits',
+                'Booking.KitID', '=', 'Kits.ID')
+            ->join('KitState',
+                'KitState.ID', '=', 'Kits.KitState')
+            ->join('KitTypes',
+                'KitTypes.ID', '=', 'Kits.KitType')
+            ->join('Branches', function($join)
+            {
+                $join->on('Kits.AtBranch', '=', 'Branches.BranchID');
+                     //->where('Booking.KitID', '=', 'Kits.ID');
+            })
+
             ->where('Booking.ForBranch', $index['branch'], 0)
+            ->select('Booking.*', 'BookingDetails.*', 'Kits.AtBranch', 'Kits.KitState', 'Kits.KitDesc', 'KitState.StateName', 'KitTypes.Name')
             ->get();
 
         $perPage = 3;
         $currentPage = Input::get('page', 1) - 1;
         $pagedData = array_slice($receiveKits, $currentPage * $perPage, $perPage);
         $data = Paginator::make($pagedData, count($receiveKits), $perPage);
-
-		return CheckIfAuthenticated('members.receivekit',[ 'selected_menu' => 'main-menu-receive', 'receiveKits' => $data], 'home.index', [], false);
+        //return $receiveKits;
+		return CheckIfAuthenticated('members.receivekit',[ 'branch_name' => $branch->Name, 'selected_menu' => 'main-menu-receive', 'receiveKits' => $data], 'home.index', [], false);
 	}
+
+    /* public function getKitInfo()
+    {
+        $index = Input::get('ID');
+
+        return DB::table('Booking')
+            ->join('BookingDetails',
+                'Booking.id', '=', 'BookingDetails.BookingID')
+            ->where('Booking.KitID', $index)
+            ->get();
+    }
+    */
 }
--- app/views/members/receivekit.blade.php
+++ app/views/members/receivekit.blade.php
@@ -5,39 +5,40 @@
 
 @section('content')
 <div>
-<h2> Receiving Branch</h2>
+<h2> {{ $branch_name }} Kits Awaiting Reception</h2>
 
 <table cellpadding = "0" class = "receive-table">
     <tr class = "receive-table-row">
         <th class = "receive-table-heading"> Booking ID</th>
-        <th class = "receive-table-heading"> Type of Kit</th>
-        <th class = "receive-table-heading"> Location</th>
-        <th class = "receive-table-heading"> Next Destination</th>
+        <th class = "receive-table-heading"> Kit Type</th>
+        <th class = "receive-table-heading"> Notes</th>
+        <th class = "receive-table-heading"> Current Location</th>
         <th class = "receive-table-heading"> Previous Destinations</th>
-        <th class = "receive-table-heading"> Start of Booking</th>
-        <th class = "receive-table-heading"> End of Booking</th>
-        <th class = "receive-table-heading"> Purpose</th>
+        <th class = "receive-table-heading"> Book Start</th>
+        <th class = "receive-table-heading"> End Book</th>
         <th class = "receive-table-heading"> Status</th>
         <th class = "receive-table-heading"> Option</th>
     </tr>
     @foreach ($receiveKits as $receiveKit)
     <tr class = "receive-table-row">
         <td class = "receive-table-bookid">{{$receiveKit->ID}}</td>
-        <td class = "receive-table-kittype">{{$receiveKit->KitID}}</td> 
+        <td class = "receive-table-kittype">{{$receiveKit->Name}}</td> 
+        <td class = "receive-table-purpose">{{$receiveKit->Purpose}}</td>
         <td class = "receive-table-location">{{$receiveKit->KitID}}</td>
-        <td class = "receive-table-nextdest">{{$receiveKit->KitID}}</td>
         <td class = "receive-table-prevdests">{{$receiveKit->KitID}}</td>
         <td class = "receive-table-startdate">{{$receiveKit->StartDate}}</td>
         <td class = "receive-table-enddate">{{$receiveKit->EndDate}}</td>
-        <td class = "receive-table-purpose">{{$receiveKit->Purpose}}</td>
-        <td class = "receive-table-kitstatus">{{$receiveKit->KitID}}</td>
+        <td class = "receive-table-kitstatus">{{$receiveKit->StateName}}</td>
         <td class = "receive-table-confirm">{{ Form::submit('Verify') }}
 
         </tr>
     @endforeach
-    </table>
+    </table>        
 </div>
 <div class = "pagination">
+@if (0 == (count($receiveKits)))
+    No kits are booked for this branch at the moment.
+@endif
 {{ $receiveKits->links() }}
 </div>
 @stop
