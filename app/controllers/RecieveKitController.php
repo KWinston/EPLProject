bfc68f9b4ac6009296d4cfb05aafc3cc075f2099
Winston Kouch <KWinston05@gmail.com>
2015-03-10 09:41:41 -0600

    Remodel Receive Kit, still need to set up queries

--- app/controllers/RecieveKitController.php
+++ app/controllers/RecieveKitController.php
@@ -5,6 +5,19 @@ class RecieveKitController extends BaseController {
 
 	public function index()
 	{
-		return CheckIfAuthenticated('members.receivekit',[ 'selected_menu' => 'main-menu-receive'], 'home.index', [], false);
+        $index = Session::All();
+
+        $receiveKits = DB::table('Booking')
+            ->join('BookingDetails',
+                'Booking.ID', '=', 'BookingDetails.BookingID')
+            ->where('Booking.ForBranch', $index['branch'], 0)
+            ->get();
+
+        $perPage = 3;
+        $currentPage = Input::get('page', 1) - 1;
+        $pagedData = array_slice($receiveKits, $currentPage * $perPage, $perPage);
+        $data = Paginator::make($pagedData, count($receiveKits), $perPage);
+
+		return CheckIfAuthenticated('members.receivekit',[ 'selected_menu' => 'main-menu-receive', 'receiveKits' => $data], 'home.index', [], false);
 	}
 }
