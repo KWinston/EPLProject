9fa6420ead2a35bbc57f0f09a66b172d824a9acc
Winston Kouch <KWinston05@gmail.com>
2015-03-18 22:43:34 -0600

    Receive Kit touched up and test-ready

--- app/controllers/RecieveKitController.php
+++ app/controllers/RecieveKitController.php
@@ -5,15 +5,15 @@ class RecieveKitController extends BaseController {
 
 	public function index()
 	{
-        $index = Session::All();
-        $post = Input::except('ID');
         $branch = Branches::find(Session::get('branch'));
 
         $receiveKits = DB::table('Booking')
             ->join('BookingDetails',
                 'Booking.ID', '=', 'BookingDetails.BookingID')
-            ->join('Kits',
-                'Booking.KitID', '=', 'Kits.ID')
+            ->join('Kits', function($join)
+            {
+                $join->on('Booking.KitID', '=', 'Kits.ID');
+            })
             ->join('KitState',
                 'KitState.ID', '=', 'Kits.KitState')
             ->join('KitTypes',
@@ -23,26 +23,39 @@ class RecieveKitController extends BaseController {
                 $join->on('Kits.AtBranch', '=', 'Branches.ID');
             })
                         
-            ->where('Booking.ForBranch', $index['branch'])
+            ->where('Booking.ForBranch', $branch->ID)
             ->select('Booking.*', 'BookingDetails.*', 'Kits.AtBranch', 'Kits.KitState', 'Kits.KitDesc',
                 'KitState.StateName', 'KitTypes.Name', 'Branches.Name As BName')
             ->orderBy('Booking.StartDate')
             ->orderBy('BookingID')
             ->get();
+/* Need history of AtBranch stored in bookings? then populate last 3 non-null? or just use last 3 ForBranch
+        $prevbranch = DB::table('Booking')
+            ->join('BookingDetails',
+                'Booking.ID', '=', 'BookingDetails.BookingID')
+            ->join('Kits',
+                    'Booking.KitID', '=', 'Kits.ID')
+            ->join('Branches', function($join)
+            {
+                $join->on('Kits.AtBranch', '=', 'Branches.ID');
+            })
+            ->select('Booking.ID', 'Booking.KitID', 'Kits.AtBranch', 'Booking.StartDate',)
+            ->orderBy('Booking.KitID', 'asc')
+            ->orderBy('Booking.StartDate', 'desc')
+            ->get();
+            */
 
         $perPage = 5;
         $currentPage = Input::get('page', 1) - 1;
         $pagedData = array_slice($receiveKits, $currentPage * $perPage, $perPage);
         $data = Paginator::make($pagedData, count($receiveKits), $perPage);
-        
-		return CheckIfAuthenticated('members.receivekit',[ 'branch_name' => $branch->Name,
+
+		return CheckIfAuthenticated('members.receivekit',[ 'branch' => $branch,
                 'selected_menu' => 'main-menu-receive', 'receiveKits' => $data], 'home.index', [], false);
 	}
 
     public function findKit($bookingID)
     {
-        $index = Session::All();
-        $post = Input::except('ID');
         $branch = Branches::find(Session::get('branch'));
 
         $receiveKits = DB::table('Booking')
@@ -59,7 +72,7 @@ class RecieveKitController extends BaseController {
                 $join->on('Kits.AtBranch', '=', 'Branches.ID');
             })
                         
-            ->where('Booking.ForBranch', $index['branch'])
+            ->where('Booking.ForBranch', $branch->ID)
             ->where('Booking.ID', $bookingID)
             ->select('Booking.ID As BookingID', 'Booking.StartDate', 'Booking.EndDate', 'Booking.KitID As KitID',
                 'BookingDetails.*', 'Kits.AtBranch', 'Kits.KitState', 'Kits.KitDesc', 'KitState.StateName',
@@ -71,7 +84,7 @@ class RecieveKitController extends BaseController {
         $pagedData = array_slice($receiveKits, $currentPage * $perPage, $perPage);
         $data = Paginator::make($pagedData, count($receiveKits), $perPage);
 
-        return CheckIfAuthenticated('members.receivekit',[ 'branch_name' => $branch->Name,
+        return CheckIfAuthenticated('members.receivekit',[ 'branch' => $branch,
                 'selected_menu' => 'main-menu-receive', 'receiveKits' => $data], 'home.index', [], false);
     }
 
@@ -81,7 +94,6 @@ class RecieveKitController extends BaseController {
             return "not a json request";
 
         $post = Input::except('ID');
-        $index = Input::get('ID');
 
         $stat = DB::table('Kits')
             ->where('id', $post['KitID'])
