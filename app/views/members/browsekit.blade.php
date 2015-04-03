57388b78114dacffac81e46a0ae45be740727285
Wilson Ho <who@ualberta.ca>
2015-04-03 00:23:10 -0600

    resized help home screen image and removed attempt at browse tooltip

--- app/views/members/browsekit.blade.php
+++ app/views/members/browsekit.blade.php
@@ -1,47 +1,17 @@
 @extends('layouts.master')
-@section('head')
-@stop
 
+@section('head')
+
+@stop
 
 @section('content')
 
 @foreach ($kits as $kit)
-<style>
-.ui-tooltip
-{
-	width:500px;
-	min-width:500px;
-}
-.ui-tooltip-content
-{
-}
 
-</style>
+<div class=browse-kit-block id="tooltip">
+	{{ $kit->Name }}
+</div>
 
-    <div class=browse-kit-block>
-        {{ $kit->Name }}
-    </div>
-    <script>
-// function makeKitToolTip("$kit")
-// {
-//     str = "<table class='tooltip-branch'>";
-//     str += "<tr>";
-//     str += "<td class='tooltip-header'>Name</td>";
-//     str += "<td class='tooltip-value'>" + branches[branchID].Name + "</td>";
-//     str += "</tr>";
-//     str += "<tr>";
-//     str += "<td class='tooltip-header'>Address</td>";
-//     str += "<td class='tooltip-value'>" + branches[branchID].Address + "</td>";
-//     str += "</tr>";
-//     str += "<tr>";
-//     str += "<td class='tooltip-header'>Phone</td>";
-//     str += "<td class='tooltip-value'>" + branches[branchID].PhoneNumber + "</td>";
-//     str += "</tr>";
-//     str += "</table>";
-//     return str;
-// }
-    </script>
 @endforeach
-
 
 @stop
